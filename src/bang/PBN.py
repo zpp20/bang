339c1d28c76e938a74eb2c419be2e8636072f97a
RestInSleep <jasjago@gmail.com>
2024-11-14 01:53:31 +0100

    most part of the implementation of assa parsing

--- src/bang/PBN.py
+++ src/bang/PBN.py
@@ -46,33 +46,62 @@ class PBN:
     def getNpNode(self):
         return self.npNode
     
-def skip_empty_lines(lines, i):
-    while lines[i].strip() == "":
-        i += 1
-    return i
+
+
 
 def load_sbml():
     pass
 
+
 def load_assa(path):
+    
+    forbidden_chars = [' ', '\t', '\n', '\r', '\v', '\f', '&', '*', '!', '^', '/', '|', ':', '(', ')']
+    
+    def skip_empty_lines(lines, i):
+        while lines[i].strip() == "" or lines[i].strip().startswith("//"):
+            i += 1
+        return i
+    
+    def get_vars(fun):
+        vars = []
+        cur = ''
+        started = False
+        for i in range(len(fun)):
+            if fun[i] in forbidden_chars:
+                if started:
+                    vars.append(cur)
+                    cur = ''
+                    started = False
+                else:
+                    continue
+            else:
+                cur += fun[i]
+                started = True
+        if started:
+            vars.append(cur)
+        return vars
+
     with open(path, 'r') as f:
         lines = f.readlines()
         no_of_lines = len(lines)
-       
+
         i = 0
         i = skip_empty_lines(lines, i)
-        
+
+        # Read the type of the PBN
         type_line = lines[i]
         type_line = type_line.strip()
         if type_line.startswith("type=") == False:
             raise ValueError("Invalid file format")
         type = type_line.split("=")[1]
-        if type not in ['synchronous', 'rog', 'rmg', 'rmgrm', 'rmgro', 'rmgrorm', 'aro']
+        if type not in ['synchronous', 'rog', 'rmg', 'rmgrm', 'rmgro', 'rmgrorm', 'aro']:
             raise ValueError("Invalid file format")
-        
+
+        print('OK')
         i += 1
         i = skip_empty_lines(lines, i)
-        
+
+        # Read the number of nodes
         n_line = lines[i]
         n_line = n_line.strip()
         if n_line.startswith("n=") == False:
@@ -84,7 +113,8 @@ def load_assa(path):
 
         i += 1
         i = skip_empty_lines(lines, i)
-
+        print('OK')
+        # Read the perturbation rate
         perturbation_line = lines[i]
         perturbation_line = perturbation_line.strip()
         if perturbation_line.startswith("perturbation=") == False:
@@ -95,6 +125,79 @@ def load_assa(path):
         perturbation = float(perturbation)
 
         i += 1
+        i = skip_empty_lines(lines, i)
+        print('OK')
+        names_dict = {}
+        index_dict = {}
+        forbidden_names = ['or', 'not', 'and']
+        # Read the names of the nodes
+        names_line = lines[i]
+        names_line = names_line.strip()
+        if names_line != "nodeNames":
+            raise ValueError("Invalid file format")
+        i += 1
+        for j in range(n):
+            name = lines[i].strip()
+            if name in names_dict:
+                raise ValueError("Duplicate node name")
+            for char in forbidden_chars:
+                if char in name:
+                    raise ValueError("Invalid node name")
+
+            names_dict[name] = j
+            index_dict[j] = name
+            i += 1
+            print(str(j) + 'OK')
+        end_names_line = lines[i]
+        end_names_line = end_names_line.strip()
+        if end_names_line != "endNodeNames":
+            raise ValueError("Invalid file format")
+        i += 1
+        i = skip_empty_lines(lines, i)
+        print('OK')
+        for j in range(n):
+            function_count = 0
+            node_line = lines[i]
+            node_line = node_line.strip()
+            if node_line != "node " + index_dict[j]:
+                raise ValueError("Invalid file format "  +  node_line)
+            i += 1
+            while lines[i].strip() != "endNode":
+                function_count += 1
+                function_line = lines[i]
+                function_line = function_line.strip()
+                function_line = function_line.split(":")
+                aux_f_line = []
+                for f in function_line:
+                    aux_f_line.append(f.strip())
+                function_line = aux_f_line
+
+                fun_prob = function_line[0]
+                try :
+                    fun_prob = float(fun_prob)
+                except ValueError:
+                    raise ValueError("Invalid file format")
+                fun_prob = float(fun_prob)
+                fun = function_line[1]
+                vars = get_vars(fun)
+                for var in vars:
+                    if var not in names_dict:
+                        raise ValueError("Invalid file format")
+                print(vars)
+                i +=1
+            i += 1
+
+        pass
+
+                
+                
+
+
+
+
+
+
+
         pass
         # nf = list(map(int, lines[1].split()))
         # nv = list(map(int, lines[2].split()))
