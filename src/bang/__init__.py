385a90c8c1c084d9a49b5bd6561be46db7b5577b
Paweł Zając <pavlzayac@gmail.com>
2024-12-03 10:17:15 +0100

    Reorganize directory and update config

--- src/bang/__init__.py
+++ src/bang/__init__.py
@@ -1,7 +1,4 @@
-from __future__ import annotations
+from ._gpu_stable import german_gpu_run
+from .core import PBN, load_from_file
 
-from ._core import add, subtract
-from ._gpu_stable import german_gpu_run, initialise_PBN
-from .PBN import PBN, load_from_file, load_assa, load_sbml
-
-__all__ = ["german_gpu_run", "add", "subtract", "initialise_PBN", "PBN", "load_from_file"]
+__all__ = ['german_gpu_run', 'PBN', 'load_from_file']
