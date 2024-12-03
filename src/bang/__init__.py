47e13418fdbaac3f8c899140a55622bb19a1fbf0
Paweł Zając <pavlzayac@gmail.com>
2024-12-04 00:24:28 +0100

    Fix missing initialise_PBN export

--- src/bang/__init__.py
+++ src/bang/__init__.py
@@ -1,4 +1,4 @@
-from ._gpu_stable import german_gpu_run
+from ._gpu_stable import german_gpu_run, initialise_PBN
 from .core import PBN, load_from_file
 
-__all__ = ['german_gpu_run', 'PBN', 'load_from_file']
+__all__ = ['german_gpu_run', 'PBN', 'load_from_file', 'initialise_PBN']
