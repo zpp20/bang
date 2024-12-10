3493e6b9a433c0fc09ad0224c6879a82d1aaf5f7
Mikolaj Czarnecki <mikolajczarnecki235@gmail.com>
2024-12-10 18:20:26 +0100

    Add source files and compiled html pages for documentation

---
+++ docs/docs_compile/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'bang'
+copyright = '2024, Pawel Zajac, Jan Jagodzinski, Bartlomiej Parapura, Mikolaj Czarnecki'
+author = 'Pawel Zajac, Jan Jagodzinski, Bartlomiej Parapura, Mikolaj Czarnecki'
+release = '10.12.2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
