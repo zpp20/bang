96e97fe798b573503bcc946d2cded70a1523b830
Paweł Zając <pavlzayac@gmail.com>
2025-03-05 22:52:59 +0100

    Chore: bump version to 0.0.1-alpha.9

--- src/bang/__init__.py
+++ src/bang/__init__.py
@@ -1,4 +1,7 @@
 from .core import PBN, load_from_file
 from .graph.graph import get_blocks
+from numba import config
+
+config.CUDA_ENABLE_PYNVJITLINK = 1 # type: ignore
 
 __all__ = ['PBN', 'load_from_file', 'get_blocks']
