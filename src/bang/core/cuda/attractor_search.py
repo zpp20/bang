dd93f467d0fe9e0017395415a5499cc7775135fb
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-03-11 20:03:47 +0100

    Added state crossing

--- src/bang/core/cuda/attractor_search.py
+++ src/bang/core/cuda/attractor_search.py
@@ -3,11 +3,29 @@ import bang.graph.graph as graph
 import numpy as np
 from itertools import product
 
+def cross_states(state1 :list[bool], nodes1 :list[int], state2 :list[bool], nodes2 :list[int]) -> list[bool]:
+    counter1, counter2 = 0, 0
+    result :list[bool] = []
+    for i in range(len(nodes1) + len(nodes2)):
+        if nodes1[counter1] < nodes2[counter2]:
+            result.append(state1[counter1])
+            counter1 += 1
+        else:
+            result.append(state2[counter2])
+            counter2 += 1
+        if counter1 == len(nodes1):
+            return result  + state2[counter2:]
+        elif counter2 == len(nodes2):
+            return result + state1[counter1:]
+            
+    return result
+
 def cross_attractors(attractor1 :list[list[bool]], nodes1: list[int], 
                      attractor2 :list[list[bool]], nodes2: list[int]) -> tuple[list[list[bool]], list[int]]:
-    result = nodes1 + nodes2
-    result.sort()
-    return [x + y for x,y in product(attractor1, attractor2)], result
+    result_nodes = nodes1 + nodes2
+    result_nodes.sort()
+            
+    return [cross_states(x, nodes1, y,  nodes2) for x,y in product(attractor1, attractor2)], result_nodes
 
 def find_attractors_realisation(network :PBN, initial_states :list[list[bool]], nodes :list[int]) -> list[list[list[bool]]]:
     def convert(state):
