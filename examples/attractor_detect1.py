f8c4cc22b528e9e443c1f86d4f5ce9a4c9ccbed3
Mikolaj Czarnecki <mikolajczarnecki235@gmail.com>
2025-03-12 23:53:19 +0100

    add quick fix for history and segment attractor method

--- examples/attractor_detect1.py
+++ examples/attractor_detect1.py
@@ -4,18 +4,22 @@ import bang
 #x_1 <- x_2
 n = 2
 nf = [1,1]
-nv = [1,2]
-F = [[True, False],[False, False, True, True]]
-varFInt = [[1],[0,1]]
+nv = [1,1]
+F = [[False, True],[True, False]]
+varFInt = [[0],[1]]
 cij = [[1.],[1.]]
 perturbation = 0.
 npNode = [0,1,2]
-n_parallel = 3
+n_parallel = 4
 
 pbn = bang.PBN(n,nf,nv,F,varFInt,cij,perturbation,npNode,n_parallel)
 
-initial_states = [[False, False], [True, False], [False, True]]
+initial_states = [[False, False],[True, False], [False, True], [True, True]]
 
-attractor = pbn.detect_attractor(initial_states)
+attractor, history = pbn.detect_attractor(initial_states)
 
-print(attractor)
+print("attractor states - ", attractor)
+
+print("trajectories: ", history)
+
+print(pbn.segment_attractor(attractor, history))
