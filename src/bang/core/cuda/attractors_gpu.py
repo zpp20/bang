07dea34e02c9b2493299efb8f5e3a5bfd7e0e910
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-03-27 10:24:22 +0100

    Implemented basic divide_and_conquer_gpu

--- src/bang/core/cuda/attractors_gpu.py
+++ src/bang/core/cuda/attractors_gpu.py
@@ -7,15 +7,30 @@ import threading
 def cross_attractors_gpu():
     pass
 
-def calculate_realisations_gpu(starting):
+def calculate_realisations_gpu():
+    pass
+
+def block_thread(sempaphore : threading.Semaphore, parent_semaphores :list[threading.Semaphore], starting: bool):
+    pass
+
+def get_elementary_blocks(blocks :list[tuple[list[int], list[int]]]) -> list[int]:
     pass
 
 def divide_and_counquer_gpu(network : PBN):
     PBN_graph = graph.Graph_PBN(network)
     PBN_graph.find_scc_and_blocks()
     blocks :list[tuple[list[int], list[int]]] = PBN_graph.blocks
+    starting_blocks = get_elementary_blocks(blocks)
     semaphores = [threading.Semaphore(len(children)) for block, children in blocks]
     attractors :list[tuple[list[list[list[bool]]], list[int]]] = []
-    max :int = 0
-    result = None
-    return result
+    threads :list[threading.Thread] = []
+    
+    for i in range(len(blocks)):
+        children, block = blocks[i]
+        threads.append(threading.Thread(target=block_thread, args=(semaphores[i], [semaphores[j] for j in children], i in starting_blocks)))
+    for t in threads:
+        t.start()
+        
+    for t in threads:
+        t.join()
+    pass
