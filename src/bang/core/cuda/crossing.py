c2ffc842d1c659716cbd7562e022d9eac3963edd
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-03-31 21:28:55 +0200

    Implemented kernel for crossing atractors

---
+++ src/bang/core/cuda/crossing.py
@@ -0,0 +1,59 @@
+import numpy as np
+import numpy.typing as npt
+from numba import cuda
+
+def corss_attractors_gpu(attractor_list :list[npt.NDArray[np.int32]], nodes :list[list[int]], attractor_start :list[int], blocks_indices :list[int]):
+    attractors_global = [cuda.to_device(attractor) for attractor in attractor_list]
+    attractors_len = cuda.to_device([attractor.shape[0] for attractor in attractor_list])
+    attractors_index = cuda.to_device(np.array(attractor_start))
+    
+    pass
+
+def increment_states(current_states, max_vals) -> bool:
+    current_states[0][0] += 1
+    cf :int
+    for i in range(len(max_vals) - 1):
+        cf = current_states[i][0] / max_vals
+        if cf != 0 and i == len(max_vals) - 1:
+            break
+        current_states[i][0] %= max_vals[i]
+        current_states[i + 1][0] += cf
+    return current_states[len(max_vals) - 1][0] == max_vals[len(max_vals) - 1]
+
+
+# Only to be called with the number of threads equal to the number of combinations of attractors to be crossed
+cuda.jit
+def cross_attractors(
+    attractors_global, #list of 2d arrays
+    block_indices, 
+    attractors_cum_index,
+    select_order,
+    result,
+    result_start,
+    result_end
+    ):
+    comb_id :int = 1024 * cuda.blockIdx.x + cuda.threadIdx.x # type: ignore 
+    tmp = comb_id
+    indices = np.zeros(block_indices.shape)
+    lengths = indices.copy()
+    current_states = np.zeros((indices.size, 3))
+    
+    for i in range(block_indices.size):
+        indices[i] = attractors_cum_index[i] + (tmp % block_indices[i])
+        tmp /= block_indices[i]
+    for i in range(indices.size):
+        lengths[i] = attractors_global[indices[i]][0][0]
+    
+    while True:
+        new_node = np.zeros(select_order.size)
+        to_read = 0
+        for i in range(len(select_order)):
+            # reads the bit pointed to by current states and stores it as 1 or 2
+            read = (1 << current_states[select_order[i]][2]) & (attractors_global[indices[i]][current_states[select_order[i]][0]][current_states[select_order[i]][1]]) >> current_states[select_order[i]][2]
+            new_node[i] += read << to_read
+            current_states[select_order[i]][1] += (current_states[select_order[i]][1] % 32 + 1) / 32
+            current_states[select_order[i]][2] = (current_states[select_order[i]][2] + 1) % 32
+            to_read = (to_read + 1) % 32
+        result[result_start : result_end] = new_node
+        if not increment_states(current_states, lengths):
+            break
