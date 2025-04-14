966e4a5ee5c4ecfdd7b563f09067e766ec2f77fe
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-04-14 17:52:14 +0200

    Fix spaghetti code in kernel

--- src/bang/core/cuda/crossing.py
+++ src/bang/core/cuda/crossing.py
@@ -2,10 +2,15 @@ import numpy as np
 import numpy.typing as npt
 from numba import cuda
 
-def corss_attractors_gpu(attractor_list :list[npt.NDArray[np.int32]], nodes :list[list[int]], attractor_start :list[int], blocks_indices :list[int]):
+def corss_attractors_gpu(
+    attractor_list :list[npt.NDArray[np.int32]], 
+    nodes :list[list[int]], 
+    attractor_cum_index :list[npt.NDArray[np.int32]], 
+    blocks_indices :list[int]
+    ):
     attractors_global = [cuda.to_device(attractor) for attractor in attractor_list]
     attractors_len = cuda.to_device([attractor.shape[0] for attractor in attractor_list])
-    attractors_index = cuda.to_device(np.array(attractor_start))
+    attractors_index = cuda.to_device(np.array(attractor_cum_index))
     
     pass
 
@@ -24,36 +29,45 @@ def increment_states(current_states, max_vals) -> bool:
 # Only to be called with the number of threads equal to the number of combinations of attractors to be crossed
 cuda.jit
 def cross_attractors(
-    attractors_global, #list of 2d arrays
-    block_indices, 
+    attractors_global,
     attractors_cum_index,
+    block_sizes, 
     select_order,
     result,
-    result_start,
-    result_end
+    result_start
     ):
+    """
+    Crosses a set of n attractors
+    - attractors_global - a list global memory arrays each storing a list of all states in attractors in each block
+    - `attractors_cum_index` - a cummulative index going into the second coordinate of `attractors_global`,  stores indices of each attractor
+    - `block_sizes` - number of nodes in each relevant block
+    - `select_order` - order by which nodes are to be selected from blocks
+    - `result` - output array
+    - `result_start` - where each thread should start writing 
+    """
     comb_id :int = 1024 * cuda.blockIdx.x + cuda.threadIdx.x # type: ignore 
     tmp = comb_id
-    indices = np.zeros(block_indices.shape)
-    lengths = indices.copy()
-    current_states = np.zeros((indices.size, 3))
+    atttractors = np.zeros(block_sizes.size) # cum_index of each attractor we are crossing
+    lengths = atttractors.copy() # length of each attractor
+    current_states = np.zeros((block_sizes.size, 3)) # for each block: (which node are we crossing, which integer to cross next, which bit to cross next)
+    write_index :int = result_start
     
-    for i in range(block_indices.size):
-        indices[i] = attractors_cum_index[i] + (tmp % block_indices[i])
-        tmp /= block_indices[i]
-    for i in range(indices.size):
-        lengths[i] = attractors_global[indices[i]][0][0]
-    
+    for i in range(block_sizes.size):
+        atttractors[i] = attractors_cum_index[(tmp % block_sizes[i])]
+        lengths[i] = attractors_cum_index[(tmp % block_sizes[i]) + 1] - attractors_cum_index[(tmp % block_sizes[i])]
+        tmp /= block_sizes[i]
+        
     while True:
         new_node = np.zeros(select_order.size)
         to_read = 0
         for i in range(len(select_order)):
             # reads the bit pointed to by current states and stores it as 1 or 2
-            read = (1 << current_states[select_order[i]][2]) & (attractors_global[indices[i]][current_states[select_order[i]][0]][current_states[select_order[i]][1]]) >> current_states[select_order[i]][2]
+            read = 1 & (attractors_global[select_order[i]][current_states[select_order[i]][0]][current_states[select_order[i]][1]]) >> current_states[select_order[i]][2]
             new_node[i] += read << to_read
             current_states[select_order[i]][1] += (current_states[select_order[i]][1] % 32 + 1) / 32
             current_states[select_order[i]][2] = (current_states[select_order[i]][2] + 1) % 32
             to_read = (to_read + 1) % 32
-        result[result_start : result_end] = new_node
+        result[write_index][:] = new_node
+        write_index += 1
         if not increment_states(current_states, lengths):
             break
