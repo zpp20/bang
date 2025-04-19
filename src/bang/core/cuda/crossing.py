3b3c48d0a79e429568849802a14d68d8d09630ef
Bartek Parapura <b.parapura@student.uw.edu.pl>
2025-04-19 10:16:53 +0200

    Changed the way states are represented to fit with existing methods.

--- src/bang/core/cuda/crossing.py
+++ src/bang/core/cuda/crossing.py
@@ -3,12 +3,17 @@ import numpy.typing as npt
 from numba import cuda
 from functools import reduce
 from operator import mul
-
-def get_select_order(nodes :list[list[int]]):
-    result = []
-    for i in range(len(nodes)):
-        result.extend(zip(nodes[i], [i] * len(nodes[i])))
-    return map(lambda x : x[0], sorted(result))
+     
+def increment_states(current_states, max_vals) -> bool:
+    current_states[0] += 1
+    cf :int
+    for i in range(len(max_vals) - 1):
+        cf = current_states[i] / max_vals
+        if cf != 0 and i == len(max_vals) - 1:
+            break
+        current_states[i] %= max_vals[i]
+        current_states[i + 1] += cf
+    return current_states[len(max_vals) - 1][0] == max_vals[len(max_vals) - 1]
      
 def get_result(attractor_cum_index :list[npt.NDArray[np.int32]]):
     cum_result :list[int] = []
@@ -19,16 +24,6 @@ def get_result(attractor_cum_index :list[npt.NDArray[np.int32]]):
         max_lens.append(indices.size)
         current.append(0)
     
-    def increment_states(current_states, max_vals) -> bool:
-        current_states[0] += 1
-        cf :int
-        for i in range(len(max_vals) - 1):
-            cf = current_states[i] / max_vals
-            if cf != 0 and i == len(max_vals) - 1:
-                break
-            current_states[i] %= max_vals[i]
-            current_states[i + 1] += cf
-        return current_states[len(max_vals) - 1][0] == max_vals[len(max_vals) - 1]
     while True:
         tmp = reduce(mul, [attractor_cum_index[i][current[i]] - (attractor_cum_index[i][current[i] - 1] if current[i] > 0 else 0) for i in range(len(attractor_cum_index))])
         result_size += tmp
@@ -45,7 +40,7 @@ def corss_attractors_gpu(
     attractors_global = [cuda.to_device(attractor) for attractor in attractor_list]
     attractors_index = [cuda.to_device(attractor_cum_index[i]) for i in range(len(attractor_cum_index))]
     blocks_sizes = cuda.to_device(attractor.shape[0] for attractor in attractor_list)
-    select_order = cuda.to_device(get_select_order(nodes))
+    nodes_global = cuda.to_device(sorted(sum(nodes, [])))
     cum_result, result_size, threads = get_result(attractor_cum_index)
     result = np.zeros((result_size, 1))
     attractors = cuda.to_device(result)
@@ -53,22 +48,11 @@ def corss_attractors_gpu(
         attractors_global,
         attractor_cum_index,
         blocks_sizes,
-        select_order,
+        nodes_global,
         attractors,
         cuda.to_device(cum_result)
     )
     return attractors
-
-def increment_states(current_states, max_vals) -> bool:
-    current_states[0][0] += 1
-    cf :int
-    for i in range(len(max_vals) - 1):
-        cf = current_states[i][0] / max_vals
-        if cf != 0 and i == len(max_vals) - 1:
-            break
-        current_states[i][0] %= max_vals[i]
-        current_states[i + 1][0] += cf
-    return current_states[len(max_vals) - 1][0] == max_vals[len(max_vals) - 1]
 
 
 # Only to be called with the number of threads equal to the number of combinations of attractors to be crossed
@@ -77,9 +61,10 @@ def cross_attractors(
     attractors_global,
     attractors_cum_index,
     block_sizes, 
-    select_order,
+    nodes,
     result,
-    result_start    
+    result_start,
+    bn_size
     ):
     """
     Crosses a set of n attractors
@@ -94,7 +79,7 @@ def cross_attractors(
     tmp = comb_id
     atttractors = np.zeros(block_sizes.size) # cum_index of each attractor we are crossing
     lengths = atttractors.copy() # length of each attractor
-    current_states = np.zeros((block_sizes.size, 3)) # for each block: (which node are we crossing, which integer to cross next, which bit to cross next)
+    current_states = np.zeros((block_sizes.size, )) # for each block: (which node are we crossing, which integer to cross next, which bit to cross next)
     write_index :int = result_start
     
     for i in range(block_sizes.size):
@@ -103,14 +88,12 @@ def cross_attractors(
         tmp /= block_sizes[i]
         
     while True:
-        new_node = np.zeros(select_order.size)
+        new_node = np.zeros((bn_size // 32) + 1)
         to_read = 0
-        for i in range(len(select_order)):
+        for i in range(nodes.shape[0]):
             # reads the bit pointed to by current states and stores it as 1 or 2
-            read = 1 & (attractors_global[select_order[i]][current_states[select_order[i]][0]][current_states[select_order[i]][1]]) >> current_states[select_order[i]][2]
-            new_node[i] += read << to_read
-            current_states[select_order[i]][1] += (current_states[select_order[i]][1] % 32 + 1) / 32
-            current_states[select_order[i]][2] = (current_states[select_order[i]][2] + 1) % 32
+            read = (1 << (nodes[i][1] % 32)) & (attractors_global[nodes[i][0]][current_states[nodes[i][0]]][nodes[i][1] // 32])
+            new_node[nodes[i][1] // 32] += read
             to_read = (to_read + 1) % 32
         result[write_index][:] = new_node
         write_index += 1
