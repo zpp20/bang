4e553058fd7a6231aabb719cf75a87de41045418
Paweł Zając <pavlzayac@gmail.com>
2025-04-27 23:24:01 +0200

    Fix: remove cuda.synchronize call after timing event synchronize

--- src/bang/core/PBN.py
+++ src/bang/core/PBN.py
@@ -732,8 +732,6 @@ class PBN:
         time_stop.record()
         time_stop.synchronize()
 
-        cuda.synchronize()
-
         elapsed = time_start.elapsed_time(time_stop)
 
         print(f"Elapsed kernel execution time: {elapsed:.3f} ms")
